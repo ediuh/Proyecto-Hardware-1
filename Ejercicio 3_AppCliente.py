8e816f79a50258aa74c355601ef292b56ce37e0d
ediuh <edie2566@gmail.com>
2026-05-02 19:03:50 -0400

    feat: Finaliza funcionalidad de ejecución de comandos en Winamp

--- Ejercicio 3_AppCliente.py
+++ Ejercicio 3_AppCliente.py
@@ -6,14 +6,18 @@ def CrearPuertos():
 
 puertoOrigen = serial.Serial("COM1", 9600, timeout=1)
 
-def GenerarEventoWINAPI(codigoEvento):
-    puertoOrigen.write(bytes([codigoEvento]))
-    print(f"Evento {codigoEvento} ({chr(codigoEvento)}) generado")
+dEventos = {"Previous": 0x5a,
+            "Play": 0x58,
+            "Pause": 0x43,
+            "Stop": 0x56,
+            "Next": 0x42,
+            "VolumeUp": 0x18,
+            "VolumeDown": 0x19
+			}
 
-def Pausar():
-    GenerarEventoWINAPI(0x43) # C
-    
-
+def GenerarCodigoEvento(codigoEvento):
+    puertoOrigen.write(bytes([dEventos[codigoEvento]]))    
+    print(f"Evento {codigoEvento} ({chr(dEventos[codigoEvento])}) generado")
 
 # Apariencia (claro-oscuro)
 ctk.set_appearance_mode("System") # system, light, dark
@@ -28,13 +32,30 @@ app.configure(fg_color="#e0e0e0")
 app.resizable(False, False)
 
 
-boton = ctk.CTkButton(app,
-					text="Pausar",
-					command=Pausar,
+def CrearBoton(texto, claveEvento):
+    boton = ctk.CTkButton(app,
+					text=texto,
+					command=lambda: GenerarCodigoEvento(claveEvento),
 					fg_color="green",
 					hover_color="darkgreen",
 					width=1,
 					height=1)
-boton.pack()
+    return boton
+
+botonAnterior = CrearBoton("Anterior", "Previous")
+botonReproducir = CrearBoton("Reproducir", "Play")
+botonPausar = CrearBoton("Pausar", "Pause")
+botonDetener = CrearBoton("Detener", "Pause")
+botonSiguiente = CrearBoton("Siguiente", "Next")
+botonSubirVol = CrearBoton("Subir volumen", "VolumeUp")
+botonBajarVol = CrearBoton("Bajar volumen", "VolumeDown")
+
+botonAnterior.pack()
+botonSiguiente.pack()
+botonPausar.pack()
+botonReproducir.pack()
+botonDetener.pack()
+botonBajarVol.pack()
+botonSubirVol.pack()
 
 app.mainloop()
--- Ejercicio 3_AppServidor.py
+++ Ejercicio 3_AppServidor.py
@@ -1,11 +1,43 @@
 import serial
 import time
+import win32gui
+import win32con
 
 puertoDestino = serial.Serial("COM2", 9600, timeout=1)
+handle = win32gui.FindWindow("Winamp v1.x", None)
+
+def EjecutarEvento(iCodigoEvento):
+    if handle:
+        if iCodigoEvento == 90: # Previous (Z)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40044)
+            print(f"Comando enviado: {iCodigoEvento}")
+        elif iCodigoEvento == 88: # Play (X)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40045)
+            print(f"Comando enviado: {iCodigoEvento}")
+        elif iCodigoEvento == 67: # Pause (C)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40046)
+            print(f"Comando enviado: {iCodigoEvento}")
+        elif iCodigoEvento == 86: # Stop (V)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40047)
+            print(f"Comando enviado: {iCodigoEvento}")
+        elif iCodigoEvento == 66: # Next (B)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40048)
+            print(f"Comando enviado: {iCodigoEvento}")
+        elif iCodigoEvento == 24: # Volume+ (↑)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40058)
+            print(f"Comando enviado: {iCodigoEvento}")
+        elif iCodigoEvento == 25: # Volume- (↓)
+            win32gui.SendMessage(handle, win32con.WM_COMMAND, 40059)
+            print(f"Comando enviado: {iCodigoEvento}")
+    else:
+        print("Nada enviado xd")
+
+
 
 while True:
     time.sleep(0.01)
     if puertoDestino.in_waiting > 0:
-        codigoEvento = puertoDestino.read(1)
-        codigoEvento = codigoEvento.decode()
-        print(codigoEvento)
+        bCodigoEvento = puertoDestino.read(1)
+        iCodigoEvento = bCodigoEvento[0]
+        EjecutarEvento(iCodigoEvento)
+
